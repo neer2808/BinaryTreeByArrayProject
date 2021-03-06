41ac018e7b18600f4d101caf43b64161ff69de9d
neer2808 <uic.neerajkhanna@gmail.com>
2021-03-06 11:05:57 +0530

    Construct blank Tree

--- src/BinaryTreeByArray.java
+++ src/BinaryTreeByArray.java
@@ -1,2 +1,10 @@
 public class BinaryTreeByArray {
+  int arr[]; // array declaration
+  int lastusedindex;
+
+  public BinaryTreeByArray(int capacity)
+  {
+    arr = new int[capacity]; // array creation
+    lastusedindex= 0;
+  }
 }
